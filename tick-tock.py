a1fe5f1523a9f842827c3a6ac46b51899f7c1ac3
Cypresslin <Cypresslin@users.noreply.github.com>
2015-08-21 17:05:36 +0800

    Update parser and exception hadling

--- tick-tock.py
+++ tick-tock.py
@@ -17,6 +17,7 @@ API = "/pwr/relays/"
 ACTION = "&value="
 PASSWD = "123456"
 RELAYS = 16
+TIMEOUT = 1
 
 
 class colors:
@@ -35,6 +36,7 @@ STAT = [colors.r + "OFF" + colors.end,
 
 def _print_relay():
     """Print the relay number sequentially."""
+    print "Query with timeout={} seconds on each relay".format(TIMEOUT)
     output = "Relay:"
     for i in range(1, RELAYS + 1, 1):
         output += ("%2i |" % i)
@@ -43,15 +45,23 @@ def _print_relay():
 
 def _print_status():
     """Print the relay status sequentially."""
+    err_flag = False
     output = "Stat: "
     for i in range(1, RELAYS + 1, 1):
         try:
             url = IP + API + '{}?ac={}'.format(i, PASSWD)
-            response = json.loads(urllib2.urlopen(url).read())['v']
+            response = json.loads(urllib2.urlopen(url, timeout=TIMEOUT).read())['v']
             output += STAT[response] + "|"
-        except:
+        except urllib2.URLError, e:
+            err_flag = True
             output += (colors.y + "ERR" + colors.end + "|")
+            sys.stdout.write("\r%s" % output)
+        except KeyboardInterrupt:
+            sys.exit(0)
     sys.stdout.write("\r%s" % output)
+    if err_flag:
+        print("\nError: " + str(e.reason))
+        sys.exit(1)
 
 
 def status():
@@ -61,15 +71,15 @@ def status():
     print("")
 
 
-def monitor():
+def monitor(rate):
     """Live update for the relay status."""
-    print("Ctrl + C to exit (update in 2 seconds)")
+    print("Ctrl + C to exit (update in %i seconds)" % rate)
     _print_relay()
     while True:
         try:
             _print_status()
             sys.stdout.flush()
-            time.sleep(1)
+            time.sleep(rate)
         except KeyboardInterrupt:
             print("")
             break
@@ -82,40 +92,49 @@ def activate(targets):
             print("ERR: Relay number %i out of range, skipping" % i)
             continue
         url = IP + API + "{}?ac={}".format(i, PASSWD)
-        response = 1 - json.loads(urllib2.urlopen(url).read())['v']
-        url = IP + API + "{}?ac={}".format(i, PASSWD) + ACTION + str(response)
-        response = json.loads(urllib2.urlopen(url).read())['v']
-        print("Relay %i is now in %s stat" % (i, STAT[response]))
+        try:
+            response = 1 - json.loads(urllib2.urlopen(url).read())['v']
+            url = IP + API + "{}?ac={}".format(i, PASSWD) + ACTION + str(response)
+            response = json.loads(urllib2.urlopen(url).read())['v']
+            print("Relay %i is now in %s stat" % (i, STAT[response]))
+        except urllib2.URLError, e:
+            print("Error: " + str(e.reason))
+            break
 
 
 def main():
     """Argument parser and major tasks here."""
     parser = argparse.ArgumentParser(description="Ethernet relay board toybox")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('targets',
-                       default=[],
-                       action='store',
-                       type=int,
-                       nargs='*',
-                       help="Flip the status of relay #")
-    group.add_argument('-s', '--status',
-                       dest='cmd',
-                       action='store_const',
-                       const=status,
-                       help="Query relay status")
-    group.add_argument('-m', '--monitor',
-                       dest='cmd',
-                       action='store_const',
-                       const=monitor,
-                       help="Keep watching relay status")
+    subparsers = parser.add_subparsers(dest='subparser_name')
+    monitor_parser = subparsers.add_parser('monitor',
+                                           help="Keep watching relay status")
+    monitor_parser.set_defaults(func=monitor)
+    monitor_parser.add_argument('-r', '--refresh',
+                                dest='idx',
+                                metavar='T',
+                                default=2,
+                                type=int,
+                                help="Refresh rate (T seconds)")
+    stat_parser = subparsers.add_parser('status',
+                                        help="Query the status of all relays")
+    stat_parser.set_defaults(func=status)
+    flip_parser = subparsers.add_parser('flip',
+                                        help="Invert the status of certain relay(s)")
+    flip_parser.set_defaults(func=activate)
+    flip_parser.add_argument('idx',
+                             default=[],
+                             nargs='*',
+                             type=int,
+                             help="The index of relay(s)")
     args = parser.parse_args()
-    if args.cmd is None and not args.targets:
+
+    if args.func.func_name is None:
         parser.parse_args(['-h'])
     else:
-        if not args.targets:
-            args.cmd()
+        if args.func.func_name is not 'status':
+            args.func(args.idx)
         else:
-            activate(args.targets)
+            args.func()
 
 
 if __name__ == "__main__":
